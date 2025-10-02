39660aa13a8e32c0cca59644a1ae5078d1727af2
Rajat Sharma <rajat9.lyfe@gmail.com>
2025-10-02 21:22:43 +0530

    cors url updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cookieparser());
 app.use(cors({
-    origin: "https://prime-notes.netlify.app",
+    origin: process.env.CORS_URL,
     credentials: true
 }));
 app.use(express.json());
