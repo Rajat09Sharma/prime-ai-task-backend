50ee5f1800717bbd6d5ecf7ce10c8677ccf56712
Rajat Sharma <rajat9.lyfe@gmail.com>
2025-10-02 21:31:40 +0530

    access control middleware added

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cookieparser());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", process.env.CORS_URL);
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    next();
+});
 app.use(cors({
     origin: process.env.CORS_URL,
     credentials: true
